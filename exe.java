7e32211fa326fac04ac67876d7990b0752bd58fa
meetcodex <42995400+meetcodex@users.noreply.github.com>
2018-09-05 13:31:27 +0530

    Add files via upload

---
+++ exe.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class exe {
+	public static void main (String arg[] )
+	{
+		Scanner obj = new Scanner(System.in);
+		String s1=obj.nextLine();
+		System.out.println(s1);
+	}
+}
+	
